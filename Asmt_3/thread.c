5248f3a3cfd588f1d6d7eab300451a1869b118c5
Thomas Foulds <tcf9bj@virginia.edu>
2013-02-16 16:02:09 -0500

    Added headers to files.

---
+++ Asmt_3/thread.c
@@ -0,0 +1,7 @@
+/* Thomas Foulds (tcf9bj)
+ * 02/16/13
+ * thread.c
+ */
+
+#include "thread.h"
+
---
+++ Asmt_3/thread.h
@@ -0,0 +1,10 @@
+/* Thomas Foulds (tcf9bj)
+ * 02/16/13
+ * thread.h
+ */
+
+#ifndef THREAD_H
+#define THREAD_H
+
+
+#endif

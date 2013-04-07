9720c869cb76b8d1b80438b8df6de878256346c7
ajr2fu <ajr2fu@virginia.edu>
2013-04-07 16:36:11 -0300

    Create ajrwhales.c
    Primitive implementation of whale mating problem; using three semaphores. Modeled loosely after the H20 example.

---
+++ Asmt_5/whales/ajrwhales.c
@@ -0,0 +1,174 @@
+#define _BSD_SOURCE  // usleep is from BSD
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+#include <sys/time.h>
+
+/**
+ * the output strings MUST be of the form:
+ * (t=TIME) WHALE_ID: ACTION
+ * 
+ * with NO leading or trailing spaces
+ * 
+ * TIME - the value of elapsed_time()
+ * WHALE_ID - the id passed into Male(int id) and Female(int id)
+ * ACTION - one of the followoing exact phrases (with no leading
+ *					or trailing spaces):
+ *
+ *	Male Created
+ *	Female Created
+ *	Found Mate
+ *	Became MatchMaker
+ *	Mated
+ *
+ */
+
+static pthread_mutex_t stdout_lock;
+
+#define LOCKED_PRINTF(fmt, args...) \
+	pthread_mutex_lock(&stdout_lock); \
+	printf(fmt, args); \
+	fflush(stdout); \
+	pthread_mutex_unlock(&stdout_lock)
+
+static struct timeval time0;
+
+double elapsed_time() {
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+
+	return (tv.tv_sec - time0.tv_sec + (tv.tv_usec - time0.tv_usec) / 1.e6 );
+}
+
+sem_t male_avail;
+sem_t fem_avail;
+sem_t maker_avail;
+int male;
+int female;
+int matchmaker;
+
+
+void create_male(int id) {
+	LOCKED_PRINTF("(t=%f) %d: %s\n", elapsed_time(), id, "Male Created");
+
+	//is a pair ready to be matched?
+	if((male > 0) && (female > 0))
+	{
+		matchmaker++;
+		signal(maker_avail);
+		LOCKED_PRINTF("(t=%f) %d: %s\n", elapsed_time(), id, "Became MatchMaker");
+		LOCKED_PRINTF("(t=%f) %d: %s\n", elapsed_time(), id, "Mated");
+		male--;
+		female--;
+	}
+	//otherwise is there a female waiting for a mate?
+	else if (female > 0)
+	{
+		male++;
+		signal(male_avail);
+		wait(maker_avail);
+	}
+	else
+	{
+		male++;
+		signal(male_avail);
+		wait(fem_avail);
+		wait(maker_avail);
+	}
+}
+
+void create_female(int id) {
+	LOCKED_PRINTF("(t=%f) %d: %s\n", elapsed_time(), id, "Female Created");
+
+	//is a pair ready to be matched?
+	if((male > 0) && (female > 0))
+	{
+		matchmaker++;
+		signal(maker_avail);
+		LOCKED_PRINTF("(t=%f) %d: %s\n", elapsed_time(), id, "Became MatchMaker");
+		LOCKED_PRINTF("(t=%f) %d: %s\n", elapsed_time(), id, "Mated");
+		male--;
+		female--;
+	}
+	//otherwise is there a male waiting for a mate?
+	else if (male > 0)
+	{
+		female++;
+		signal(fem_avail);
+		wait(maker_avail);
+	}
+	else
+	{
+		female++;
+		signal(fem_avail);
+		wait(male_avail);
+		wait(maker_avail);
+	}
+}
+
+void *create_whale(void* arg) {
+	int id = *((int *) arg);
+
+	// is this a male or a female?
+	if (rand() & 1) { // odd for male
+		create_male(id);
+	} else { // even for female
+		create_female(id);
+	}
+
+	pthread_exit(NULL);
+}
+
+void whale_sleep(int wait_time_ms) {
+	usleep(wait_time_ms * 1000); // convert from ms to us
+}
+
+void usage(char *arg0) {
+	fprintf(stderr, "Usage: %s num_whales wait_time\n"
+					"\tnum_whales - the total number of whales to create\n"
+	 				"\twait_time - the amount of time to wait before creating another whale (seconds)\n",
+					arg0);
+	exit(-1);
+}
+
+int main(int argc, char *argv[]) {
+	if (argc != 3) {
+		usage(argv[0]);
+	}
+	
+	pthread_mutex_init(&stdout_lock, NULL);
+
+	// seed the random number generator
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	srand(tv.tv_usec);
+
+	gettimeofday(&time0, NULL);
+
+	int num_whales = atoi(argv[1]);
+	int wait_time_ms = atof(argv[2]) * 1000;
+
+	// initialize the semaphores
+  	sem_init(&male_avail, 0, 0);
+ 	sem_init(&fem_avail, 0, 0);
+  	sem_init(&maker_avail, 0, 0);
+
+	pthread_attr_t thread_attr;
+	pthread_attr_init(&thread_attr);
+	pthread_attr_setdetachstate(&thread_attr, PTHREAD_CREATE_DETACHED);
+
+	for (int whale = 0; whale < num_whales; whale++) {
+		whale_sleep(wait_time_ms);
+		pthread_t whale_thread_id;
+		pthread_create(&whale_thread_id, &thread_attr, create_whale, (void *) &whale);
+	}
+
+	// this should enough time to resolve the matches
+	// than can be resolved
+	whale_sleep(wait_time_ms * 10.0);
+	
+	pthread_mutex_destroy(&stdout_lock);
+}

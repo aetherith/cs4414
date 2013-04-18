4c62cac68167e50b6fcc71d48adb57a165619eff
Thomas Foulds <tcf9bj@virginia.edu>
2013-04-18 14:54:32 -0400

    Wrote methods to write direct blocks as well as I1/2 to an output file.  Having some issues with searching for additional inodes.

--- Asmt_6/recover.c
+++ Asmt_6/recover.c
@@ -16,179 +16,408 @@ static void *data_file_map_start;
 static void *data_file_map_end;
 static int max_block_number;
 
-int main ( int argc, char* argv[] )
+int
+main (int argc, char *argv[])
 {
-  if ( argc < 4 )
+  if (argc < 4)
     {
-      printf( "recover UID GID [filename]\n" );
-      exit( -1 );
+      printf ("recover UID GID [filename]\n");
+      exit (-1);
     }
 
-  search_uid = atoi( argv[1] );
+  search_uid = atoi (argv[1]);
 
-  if ( search_uid < 0 )
+  if (search_uid < 0)
     {
-      perror( "UID is < 0 and invalid." );
-      exit( -1 );
+      perror ("UID is < 0 and invalid.");
+      exit (-1);
     }
-  
-  search_gid = atoi( argv[2] );
 
-  if ( argv[2] < 0 )
+  search_gid = atoi (argv[2]);
+
+  if (argv[2] < 0)
     {
-      perror( "GID is < 0 and invalid." );
-      exit( -1 );
+      perror ("GID is < 0 and invalid.");
+      exit (-1);
     }
 
   // Open as read only to prevent damage to the datafile
-  data_file_descriptor = open( argv[3], O_RDONLY );
-  if ( data_file_descriptor == -1 )
+  data_file_descriptor = open (argv[3], O_RDONLY);
+  if (data_file_descriptor == -1)
     {
-      perror( "Couldn't open file." );
-      exit( -1 );
+      perror ("Couldn't open file.");
+      exit (-1);
     }
 
   struct stat data_file_stat;
-  fstat( data_file_descriptor, &data_file_stat );
+  fstat (data_file_descriptor, &data_file_stat);
 
-  printf("Data file size in bytes: %lu\n", data_file_stat.st_size);
-  
+  printf ("Data file size in bytes: %lu\n", data_file_stat.st_size);
 
-  data_file_map_start = mmap( NULL, data_file_stat.st_size, PROT_READ,
-                              MAP_PRIVATE, data_file_descriptor, 0 );
 
-  if ( data_file_map_start == MAP_FAILED )
+  data_file_map_start = mmap (NULL, data_file_stat.st_size, PROT_READ,
+			      MAP_PRIVATE, data_file_descriptor, 0);
+
+  if (data_file_map_start == MAP_FAILED)
     {
-      close( data_file_descriptor );
-      perror( "Memory map failed." );
-      exit( -1 );
+      close (data_file_descriptor);
+      perror ("Memory map failed.");
+      exit (-1);
     }
-  
+
   data_file_map_end = data_file_map_start + data_file_stat.st_size;
 
   max_block_number = data_file_stat.st_size / BLOCK_SIZE;
 
   void *search_ptr = data_file_map_start;
-  
+
   // Search for a new inode that forfills one of our properties
 
-  inode_t *f = find_inode( search_ptr, data_file_map_end,
-                           search_uid, search_gid );
-  // Print found inode
-  print_inode( f );
-
-  // Check that the inode is valid
-
-  check_inode( f );
-
-  // Create output file of size specified by inode and memory map it
-
-  // Memcopy direct blocks from mapped datafile to output file in order
-
-  // For each indirect block access it and gather the block #s it contains
-
-  //
-
-  // Unmap the file from memory
-  if ( munmap( data_file_map_start, data_file_stat.st_size ) == -1 )
+  inode_t *f = find_inode (search_ptr, data_file_map_end,
+			   search_uid, search_gid);
+  int file_num = 0;
+  while (f != NULL)
     {
-      perror( "Error unmapping data file." );
-      close( data_file_descriptor );
-      exit( -1 );
+      // Print found inode
+      print_inode (f);
+
+      // Check that the inode is valid
+
+      if (check_inode (f) < 0)
+	{
+	  perror ("INODE INVALID!");
+	}
+      else
+	{
+          // This buffer should be large enough to hold any filename
+          char output_file_name[15];
+          sprintf(output_file_name, "%d", file_num);
+	  FILE *output_file_ptr = fopen (output_file_name, "w");
+	  if (output_file_ptr == NULL)
+	    {
+	      perror ("Could not open output file.");
+	      exit (-1);
+	    }
+
+	  // Copy direct blocks from mapped datafile to output file in order
+          printf("Copying DBLOCKS.\n");
+	  int data_copied = 0;
+	  int dblock_pos;
+	  for (dblock_pos = 0; dblock_pos < N_DBLOCKS; dblock_pos++)
+	    {
+	      if (f->dblocks[dblock_pos] > 0)
+		{
+		  int bytes_copied = copy_block (data_file_map_start,
+						 output_file_ptr,
+						 f->dblocks[dblock_pos],
+						 f->size - data_copied);
+		  if (bytes_copied < 0)
+		    {
+		      perror ("Error copying direct blocks to output file.");
+		      exit (-1);
+		    }
+		  else
+		    {
+		      data_copied += bytes_copied;
+		    }
+		}
+	      else
+		{
+		  printf ("No more data in direct blocks!\n");
+		  break;
+		}
+	    }
+
+	  printf ("%2.2f %% copied.\n",
+		  ((float) data_copied / f->size) * 100);
+	  printf ("%i/%i\n", data_copied, f->size);
+
+	  // For each I1 block access it and gather the block #s it contains
+	  if (data_copied < f->size)
+	    {
+              printf("Copying I1BLOCKS.\n");
+	      int iblock_pos;
+	      for (iblock_pos = 0; iblock_pos < N_IBLOCKS; iblock_pos++)
+		{
+		  if (f->iblocks[iblock_pos] > 0)
+		    {
+		      int bytes_copied = copy_i1block (data_file_map_start,
+						       output_file_ptr,
+						       f->iblocks[iblock_pos],
+						       f->size - data_copied);
+		      if (bytes_copied <= 0)
+			{
+			  perror
+			    ("Error copying indirect blocks to output file.");
+			  exit (-1);
+			}
+		      else
+			{
+			  data_copied += bytes_copied;
+			}
+		    }
+		  else
+		    {
+		      printf ("No more valid indirect blocks!\n");
+		      break;
+		    }
+		}
+	    }
+
+	  printf ("%2.2f %% copied.\n",
+		  ((float) data_copied / f->size) * 100);
+	  printf ("%i/%i\n", data_copied, f->size);
+
+	  // For the I2 block access it, and build an array of the I1 blocks it
+	  // points to.
+	  // Then iterate through this array and use copy_i1block
+	  if (data_copied < f->size)
+	    {
+              printf("Copying I2BLOCK.\n");
+	      int i1block_buffer[BLOCK_SIZE / sizeof (int)];
+	      int i1block_count =
+		unwind_iblock (data_file_map_start, f->i2block,
+			       i1block_buffer);
+	      if (i1block_count < 0)
+		{
+		  perror ("Could not unwind I2BLOCK!");
+		  exit (-1);
+		}
+	      int i1block_pos;
+	      for (i1block_pos = 0; i1block_pos < i1block_count;
+		   i1block_pos++)
+		{
+		  if (i1block_buffer[i1block_pos] > 0)
+		    {
+		      int bytes_copied = copy_i1block (data_file_map_start,
+						       output_file_ptr,
+						       i1block_buffer
+						       [i1block_pos],
+						       f->size - data_copied);
+		      if (bytes_copied <= 0)
+			{
+			  perror ("Error copying I2 blocks to output file.");
+			  exit (-1);
+			}
+		      else
+			{
+			  data_copied += bytes_copied;
+			}
+		    }
+		  else
+		    {
+		      printf ("No more valid I2 blocks!\n");
+		      break;
+		    }
+		}
+	    }
+
+	  printf ("%2.2f %% copied.\n",
+		  ((float) data_copied / f->size) * 100);
+	  printf ("%i/%i\n", data_copied, f->size);
+
+	  // For the I3 block unwind it to an array of I2 blocks.  For each element
+	  // in that array unwind to an array of I1 blocks.  Copy data from I1 blocks
+	  // using copy_i1block.
+	  if (data_copied < f->size)
+	    {
+              printf("Copying I3BLOCK.\n");
+	    }
+
+	  printf ("%2.2f %% copied.\n",
+		  ((float) data_copied / f->size) * 100);
+	  printf ("%i/%i\n", data_copied, f->size);
+	}
+      search_ptr = f + 1;
+      f = find_inode(search_ptr, data_file_map_end,
+                     search_uid, search_gid);
+      file_num ++;
+    }
+
+  // Unmap the data file from memory
+  if (munmap (data_file_map_start, data_file_stat.st_size) == -1)
+    {
+      perror ("Error unmapping data file.");
+      close (data_file_descriptor);
+      exit (-1);
     }
   return 0;
 };
 
-inode_t* find_inode( void *start_ptr, void *end_ptr, int uid, int gid )
+inode_t *
+find_inode (void *start_ptr, void *end_ptr, int uid, int gid)
 {
-  printf("== find_inode ==\n");
-  printf("Searching for:\nUID: %i\nGID: %i\n", uid, gid);
+  printf("Searching for INODE.\n");
+  printf("Starting at: %p\n", start_ptr);
   int *search_ptr = (int*) start_ptr;
-  while ( search_ptr != end_ptr )
+  while (search_ptr <= end_ptr - sizeof(int))
     {
-      if ( *search_ptr == gid )
-        {
-          printf("GID match found.\n");
-          return search_ptr - 3;
-        }
-      if ( *search_ptr == uid )
-        {
-          printf("UID match found.\n");
-          // Admittedly the -2 is a magic number to make it line up properly
-          // in the inode_t structure.
+      if ((search_ptr[0] == uid) && (search_ptr[1] == gid))
+	{
           return search_ptr - 2;
-        }
+	}
       search_ptr++;
     }
-  printf("!= find_inode - NULL =!\n");
   return NULL;
 }
 
-int check_inode ( inode_t *node )
+int
+check_inode (inode_t * node)
 {
   // Test inode properties against known requirements to prevent bogus records.
   // This means checking that all block numbers are <= DF.size/BL.size
 
-  int dblock_pos = 0;
-  for ( dblock_pos; dblock_pos < N_DBLOCKS; dblock_pos++ )
+  int dblock_pos;
+  for (dblock_pos = 0; dblock_pos < N_DBLOCKS; dblock_pos++)
     {
-      if ( ( node->dblocks[dblock_pos] < 0 ) || 
-           ( node->dblocks[dblock_pos] >= max_block_number ) )
-        {
-          printf("DBLOCK OUT OF RANGE!\n");
-          return -1;
-        }
+      if ((node->dblocks[dblock_pos] < 0) ||
+	  (node->dblocks[dblock_pos] >= max_block_number))
+	{
+	  perror ("DBLOCK OUT OF RANGE!");
+	  return -1;
+	}
     }
 
-  int iblock_pos = 0;
-  for ( iblock_pos; iblock_pos < N_IBLOCKS; iblock_pos++ )
+  int iblock_pos;
+  for (iblock_pos = 0; iblock_pos < N_IBLOCKS; iblock_pos++)
     {
-      if ( ( node->iblocks[iblock_pos] < 0 ) ||
-           ( node->iblocks[iblock_pos] >= max_block_number ) )
-        {
-          printf("IBLOCK OUT OF RANGE!\n");
-          return -1;
-        }
+      if ((node->iblocks[iblock_pos] < 0) ||
+	  (node->iblocks[iblock_pos] >= max_block_number))
+	{
+	  perror ("IBLOCK OUT OF RANGE!");
+	  return -1;
+	}
     }
 
-  if ( ( node->i2block < 0 ) || ( node->i2block >= max_block_number ) )
+  if ((node->i2block < 0) || (node->i2block >= max_block_number))
     {
-      printf("I2BLOCK OUT OF RANGE!\n");
+      perror ("I2BLOCK OUT OF RANGE!");
       return -1;
     }
 
-  if ( ( node->i3block < 0 ) || ( node->i3block >= max_block_number ) )
+  if ((node->i3block < 0) || (node->i3block >= max_block_number))
     {
-      printf("I3BLOCK OUT OF RANGE!\n");
+      perror ("I3BLOCK OUT OF RANGE!");
       return -1;
     }
   return 0;
 }
 
-void print_inode ( inode_t *node )
+void
+print_inode (inode_t * node)
 {
-  if ( node == NULL )
+  if (node == NULL)
     {
-      printf( "NULL POINTER\n" );
+      printf ("NULL POINTER\n");
       return;
     }
-  printf( "foo: %i\n", node->foo );
-  printf( "nlink: %i\n", node->nlink );
-  printf( "uid: %i\n", node->uid );
-  printf( "gid: %i\n", node->gid );
-  printf( "size: %i\n", node->size );
-  printf( "ctime: %i\n", node->ctime );
-  printf( "mtime: %i\n", node->mtime );
-  printf( "atime: %i\n", node->atime );
+  printf ("address: %p\n", node);
+  printf ("foo: %i\n", node->foo);
+  printf ("nlink: %i\n", node->nlink);
+  printf ("uid: %i\n", node->uid);
+  printf ("gid: %i\n", node->gid);
+  printf ("size: %i\n", node->size);
+  printf ("ctime: %i\n", node->ctime);
+  printf ("mtime: %i\n", node->mtime);
+  printf ("atime: %i\n", node->atime);
   int i;
-  for ( i = 0; i < N_DBLOCKS; i++ )
+  for (i = 0; i < N_DBLOCKS; i++)
     {
-      printf( "dblock (%i): %i\n", i, node->dblocks[i] );
+      printf ("dblock (%i): %i\n", i, node->dblocks[i]);
     }
-  for ( i = 0; i < N_IBLOCKS; i++ )
+  for (i = 0; i < N_IBLOCKS; i++)
     {
-      printf( "iblock (%i): %i\n", i, node->iblocks[i] );
+      printf ("iblock (%i): %i\n", i, node->iblocks[i]);
     }
-  printf( "i2block: %i\n", node->i2block );
-  printf( "i3block: %i\n", node->i3block );
+  printf ("i2block: %i\n", node->i2block);
+  printf ("i3block: %i\n", node->i3block);
+};
+
+int
+copy_block (void *from_ptr, FILE * to_ptr, int block_num, int bytes)
+{
+  // printf( "== copy_block ==\n" );
+  if (bytes < 0)
+    {
+      perror ("Bytes to copy requested < 0.");
+      return -1;
+    }
+
+  if (bytes > BLOCK_SIZE)
+    {
+      bytes = BLOCK_SIZE;
+    }
+  void *copy_buffer = malloc (bytes);
+  if (copy_buffer == NULL)
+    {
+      perror ("Could not malloc copy buffer.");
+      return -1;
+    }
+  memcpy (copy_buffer, from_ptr + (BLOCK_SIZE * block_num), bytes);
+  if (fwrite (copy_buffer, 1, bytes, to_ptr) != bytes)
+    {
+      perror ("Unsuccessful block data copy!");
+      return -1;
+    }
+  free (copy_buffer);
+  //printf( "!= copy_block =!\n" );
+  return bytes;
+};
+
+int
+copy_i1block (void *from_ptr, FILE * to_ptr, int block_num, int bytes)
+{
+  //printf( "== copy_i1block ==\n" );
+  if (bytes < 0)
+    {
+      perror ("Bytes to copy requested < 0.");
+      return -1;
+    }
+  int block_buffer[BLOCK_SIZE / sizeof (int)];
+  int block_count = unwind_iblock (from_ptr, block_num, block_buffer);
+  if (block_count < 0)
+    {
+      perror ("Could not unwind I1BLOCK!");
+      return -1;
+    }
+  int block_pos;
+  int data_copied = 0;
+  for (block_pos = 0; block_pos < block_count; block_pos++)
+    {
+      int bytes_copied = copy_block (from_ptr,
+				     to_ptr,
+				     block_buffer[block_pos],
+				     bytes - data_copied);
+      if (bytes_copied < 0)
+	{
+	  perror ("Error copying direct blocks to output file.");
+	  return -1;
+	}
+      else
+	{
+	  data_copied += bytes_copied;
+	}
+    }
+  //printf( "!= copy_i1block =!\n" );
+  return data_copied;
+};
+
+int
+unwind_iblock (void *source_ptr, int block_num, int *block_buffer)
+{
+  memcpy (block_buffer, source_ptr + (BLOCK_SIZE * block_num), BLOCK_SIZE);
+  int block_pos;
+  for (block_pos = 0; block_pos < BLOCK_SIZE / sizeof (int); block_pos++)
+    {
+      if (block_buffer[block_pos] == 0)
+	{
+	  break;
+	}
+    }
+  if (block_buffer[block_pos] == 0)
+    {
+      // This is the case that there were no indirect blocks pointed to.
+      return -1;
+    }
+  return block_pos + 1;
 };
--- Asmt_6/recover.h
+++ Asmt_6/recover.h
@@ -23,37 +23,65 @@
 
 #define BLOCK_SIZE 1024
 
-#define GIF87A_SIGNATURE 0x474946383761           // .gif
-#define GIF89A_SIGNATURE 0x474946383961           // .gif
-#define TIF_SIGNATURE    0x49492A00               // .tif
-#define TIFF_SIGNATURE   0x4D4D002A               // .tiff
-#define EXE_SIGNATURE    0x4D5A                   // .exe
-#define PNG_SIGNATURE    0x89504E470D0A1A0A       // .png
-#define UTF8_SIGNATURE   0xEFBBBF                 // ASCII, .html
-#define PDF_SIGNATURE    0x25504446               // .pdf
-#define PS_SIGNATURE     0x2521                   // .ps
-#define JPG_SIGNATURE    0xFFD8FFE0               // .jpg
-#define JPEG_SIGNATURE   0x0                      // .jpeg
+#define GIF87A_SIGNATURE 0x474946383761	// .gif
+#define GIF89A_SIGNATURE 0x474946383961	// .gif
+#define TIF_SIGNATURE    0x49492A00	// .tif
+#define TIFF_SIGNATURE   0x4D4D002A	// .tiff
+#define EXE_SIGNATURE    0x4D5A	// .exe
+#define PNG_SIGNATURE    0x89504E470D0A1A0A	// .png
+#define UTF8_SIGNATURE   0xEFBBBF	// ASCII, .html
+#define PDF_SIGNATURE    0x25504446	// .pdf
+#define PS_SIGNATURE     0x2521	// .ps
+#define JPG_SIGNATURE    0xFFD8FFE0	// .jpg
+#define JPEG_SIGNATURE   0x0	// .jpeg
 
-struct inode { 
-  int foo;      /* Unknown field */
-  int nlink;    /* Number of links to this file */
-  int uid;      /* Owner's user ID */
-  int gid;      /* Owner's group ID */
-  int size;     /* Number of bytes in file */
-  int ctime;    /* Time field */
-  int mtime;    /* Time field */
-  int atime;    /* Time field */
-  int dblocks[N_DBLOCKS]; /* Pointers to data blocks */ 
-  int iblocks[N_IBLOCKS]; /* Pointers to indirect blocks */ 
-  int i2block; /* Pointer to doubly indirect block */
-  int i3block; /* Pointer to triply indirect block */
+struct inode
+{
+  int foo;			/* Unknown field */
+  int nlink;			/* Number of links to this file */
+  int uid;			/* Owner's user ID */
+  int gid;			/* Owner's group ID */
+  int size;			/* Number of bytes in file */
+  int ctime;			/* Time field */
+  int mtime;			/* Time field */
+  int atime;			/* Time field */
+  int dblocks[N_DBLOCKS];	/* Pointers to data blocks */
+  int iblocks[N_IBLOCKS];	/* Pointers to indirect blocks */
+  int i2block;			/* Pointer to doubly indirect block */
+  int i3block;			/* Pointer to triply indirect block */
 };
 
 typedef struct inode inode_t;
 
-inode_t* find_inode( void *start_ptr, void *end_ptr, int uid, int gid );
+// Finds an inode structure with either UID or GID that matches those passed
+// in the call.
+// void *start_ptr is where in the memory mapped source file the search should
+// begin from.
+// void *end_ptr is the last valid address in the memory mapped source file.
+inode_t *find_inode (void *start_ptr, void *end_ptr, int uid, int gid);
 
-void print_inode ( inode_t *node );
+// Checks inode contents against known requirements to prevent bogus records
+// by making sure all block numbers referenced are valid.
+int check_inode (inode_t * node);
+
+// Prints content of inode in a formatted fashion.
+void print_inode (inode_t * node);
+
+// Copies block data from a memory mapped file to a FILE pointer.
+// void *from_ptr should be a pointer to the beginning of the source mapping.
+// void *to_ptr should be a FILE pointer to the output file insertion point.
+// Returns number of bytes copied on successful copy, -1 otherwise.
+int copy_dblock (void *from_ptr, FILE * to_ptr, int block_num, int bytes);
+
+// Unwinds an I1 block and copies its DBLOCKS to FILE pointer.
+// Returns number of bytes copied on successful copy, -1 otherwise.
+int copy_i1block (void *from_ptr, FILE * to_ptr, int block_num, int bytes);
+
+// Follows the indirection of an I(n) block and copies the found block numbers
+// into the integer array *block_buffer.
+// Assumes that any empty pointers in the IBLOCK are zeroed, not garbage filled.
+// int *block_buffer is expected to be BLOCK_SIZE/sizeof( int )
+// Returns number of valid blocks found if successful and > 0, -1 otherwise.
+int unwind_iblock (void *source_ptr, int block_num, int *block_buffer);
 
 #endif
